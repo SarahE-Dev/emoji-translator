1d763dc2e1ae8ac6e2232e059e0f19b97770198c
Sarah E <seatherly.prsvr@gmail.com>
2023-10-02 16:13:09 -0500

    all but stretch done

---
+++ main.js
@@ -0,0 +1,54 @@
+let goforit = document.querySelector('#submit-button');
+
+goforit.addEventListener('click', ()=>{
+    let thisinput = document.querySelector('#translator-input');
+    let translateValue = thisinput.value;
+    let output = document.querySelector('#results');
+    let select = '';
+    let radioButtons = document.querySelectorAll('[name="translation-section"]');
+    for(let selection of radioButtons){
+        if(selection.checked){
+            select = selection.value
+        }
+    }
+    if(select === 'encode'){
+        output.innerText = encode(translateValue)
+    }else if(select === 'madlib'){
+        output.innerText = madlib(translateValue)
+    }else if(select === 'translate'){
+        output.innerText = translate(translateValue)
+    }else if(select === 'search'){
+        output.innerText = '';
+        let searchArray = search(translateValue);
+        for(let item of searchArray){
+            let symbol = item.symbol;
+            let newElement = document.createElement('p');
+            newElement.innerText = symbol;
+            output.appendChild(newElement);
+        }
+        if(searchArray.length === 0){
+            output.innerText = 'No emojis have been found that match your criteria.'
+        }
+    }else if(select === 'random'){
+    let number = Math.floor(Math.random() * 4) + 1;
+    if(number === 1){
+        output.innerText = encode(translateValue)
+    }else if(number === 2){
+        output.innerText = madlib(translateValue)
+    }else if(number === 3){
+        output.innerText = translate(translateValue)
+    }else if(number === 4){
+        output.innerText = '';
+        let searchArray = search(translateValue);
+        for(let item of searchArray){
+            let symbol = item.symbol;
+            let newElement = document.createElement('p');
+            newElement.innerText = symbol;
+            output.appendChild(newElement);
+        }
+        if(searchArray.length === 0){
+            output.innerText = 'No emojis have been found that match your criteria.'
+        }
+    }
+    }
+})
